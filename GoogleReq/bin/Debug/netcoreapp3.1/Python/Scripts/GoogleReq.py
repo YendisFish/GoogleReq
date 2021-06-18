e346009383e374f887f9a477c78b1646e45623e0
YendisFish <44734948+YendisFish@users.noreply.github.com>
2021-06-18 09:04:52 -0600

    2.0
    Added save file option

---
+++ GoogleReq/bin/Debug/netcoreapp3.1/Python/Scripts/GoogleReq.py
@@ -0,0 +1,25 @@
+from googlesearch import *
+import argparse
+from os import path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-results", default="10")
+args = parser.parse_args()
+
+numbers = args.results
+
+startpath = path.join(path.dirname(__file__), 'configpath.txt')
+
+with open(startpath, 'r') as config:
+    path = config.read()
+
+with open(path + "Query.txt", 'r') as f:
+    Query = f.read()
+    f.close()
+with open(path + "langsettings.txt", 'r') as f:
+    lang = f.read()
+    f.close()
+print(f"SETTINGS: {Query} : {lang}")
+
+for Result in search(Query, lang=lang, num_results=int(numbers)):
+    print(Result)
